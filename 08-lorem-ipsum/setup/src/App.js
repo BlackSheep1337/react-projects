84b6c9cd4d9b8623616edf5ea859e7aa42c2a611
Alexandre <naitwa.alexandre@gmail.com>
2021-08-11 13:52:29 -0300

    09

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [paragraph, setParagraph] = useState('');
+  const [sliceValue, setSliceValue] = useState(0);
   
-  
+  const handleClick = () => {
+    if (paragraph < 1) {
+      setSliceValue(1);
+    } else { 
+      setSliceValue(paragraph);
+    }
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM IPSUM?</h3>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          value={ paragraph }
+          onChange={ (e) => setParagraph(e.target.value) }
+          name="amount"
+          id="amount"
+          type="number"
+        />
+        <button
+          onClick={ handleClick }
+          type="button"
+          className="btn"
+        >
+          generate
+        </button>
+      </form>
+      {data.slice(0, sliceValue).map((paragraph, idx) =>  (
+        <article key={ idx }>
+          { paragraph }
+        </article>
+      ))}
+    </section>
+  )
 }
 
 export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  const baseColor = 10;
+  const hexValue = `#${hexColor}`
+
+  useEffect(() => {
+    let timeout = setInterval(() => {
+      setAlert(false); 
+    }, 1500);
+    return () => clearInterval(timeout);
+  }, [alert]);
+
+  return <article
+      className={`color ${index > baseColor && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})`}}
+      onClick={() => {
+        navigator.clipboard.writeText(hexColor)
+        .then(() => setAlert(true));
+      }}
+    >
+    <p className="percent-value">
+      {weight}%
+    </p>
+    <p className="color-value">
+      {hexValue}
+    </p>
+    <p>{alert && <span className="alert">copied to clipboard</span>}</p>
+  </article>
 }
 
 export default SingleColor
